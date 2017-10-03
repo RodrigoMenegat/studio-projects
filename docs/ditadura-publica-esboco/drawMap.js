df15933d2099417e7707c2dcbee950ba7d9101e3
RodrigoMenegat <rodrigoschuinski@gmail.com>
2017-10-03 15:53:58 -0400

    Changed background

--- docs/ditadura-publica-esboco/drawMap.js
+++ docs/ditadura-publica-esboco/drawMap.js
@@ -9,7 +9,7 @@
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
       '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
       'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    id: 'mapbox.dark'
+    id: 'mapbox.light'
   }).addTo(map);
 
   // Control that shows information on hover
