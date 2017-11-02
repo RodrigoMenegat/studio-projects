3ff60441ff40111d92af5dc1dcb90008b7755406
RodrigoMenegat <rodrigoschuinski@gmail.com>
2017-11-01 23:31:33 -0400

    Uploading project 2 for Storytelling with Data class

---
+++ docs/whatsapp-group/chart-1.js
@@ -0,0 +1,138 @@
+(function() {
+  // Set values for the margins, height and width
+  var margin = { top: 20, left: 100, right: 55, bottom: 60},
+      height = 500 - margin.top - margin.bottom,
+      width = 1000 - margin.left - margin.right;
+
+  // Append a svg to the chart-1 div.
+  var svg = d3.select("#chart-1")
+      .append("svg")
+      .attr("height", height + margin.top + margin.bottom)
+      .attr("width", width + margin.left + margin.right)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+  // Setting the scales
+  
+  // The xPositionScale will take on the year and return a linear position from 0 to width
+  // .domain will go from 10 (so we have space for the line in the last chart) to max messages, so it will be set later
+  var xPositionScale = d3.scaleLinear()
+    .range([10, width])
+
+  // The yPositionScale is a bandScale that goes in descending sort order
+  // .domain will also be set later, using d3.map() in the friend names
+  var yPositionScale = d3.scaleBand()
+    .range([0, height])
+
+  // Now let's read the data in, with our usual MAGIC INVOCATION
+  // "OH D3 OUR POWERFUL AND ALMIGHT OVERLORD, IN THY NAME I BRING THE DATA IN"
+  d3.queue()
+    .defer(d3.csv, "./data/friends.csv")
+    .await(ready);
+
+  function ready(error, datapoints) {
+    // Before everything, let's finish setting our scales
+
+    var maxMessages = d3.max(datapoints.map(function(d) {return d.message}))
+    var allFriends = datapoints.map(function(d) { return d.friend })
+    xPositionScale.domain([0, maxMessages])
+    yPositionScale.domain(allFriends)
+    console.log(datapoints)
+
+    // Adding circles
+    svg.selectAll(".circle-friend")
+      .data(datapoints)
+      .enter().append('circle')
+      .attr("r", 5)
+      .attr("fill","#DA546C")
+      .attr("stroke","black")
+      .attr("stroke-width",1.5)
+      .attr("cy", function(d){
+        return yPositionScale(d.friend)
+      })
+      .attr("cx", 0)
+      .transition()
+      .duration(1500)
+      .attr("cx", function(d){
+        return xPositionScale(d.message)
+      })
+
+  // Adding lines - note that I don't use the line generator because they are simple straight lines
+  svg.selectAll(".line-friend")
+    .data(datapoints)
+    .enter().append("line")
+    .attr("stroke","black")
+    .attr("stroke-width",1.5)
+    .attr("x1",0)
+    .attr("x2", 0)
+    .attr("y1", function(d){
+      return yPositionScale(d.friend)
+    })
+    .attr("y2", function(d) {
+      return yPositionScale(d.friend)
+    })
+    .transition()
+    .duration(1500)
+    .attr("x2", function(d) {
+      return xPositionScale(d.message) - 4
+    })
+
+
+  // Adding text to the left side of the chart
+  svg.selectAll(".label-friend")
+    .data(datapoints)
+    .enter().append("text")
+    .text(function(d) {
+      return d.friend
+    })
+    .attr("y", function(d){
+      return yPositionScale(d.friend)
+   })
+    .attr("dy", 5)
+    .attr("x", 0 - (margin.left/2) )
+    .attr("text-anchor", "middle")
+
+  // Adding text to the right side of the chart
+  svg.selectAll(".label-total")
+    .data(datapoints)
+    .enter().append("text")
+    .text(function(d) {
+      return "(" + d.message + ")"
+    })
+    .attr("y", function(d){
+      return yPositionScale(d.friend)
+    })
+    .attr("dy", 5)
+    .attr("text-anchor", "left")
+    .attr("x", 0)
+    .transition()
+    .duration(1500)
+    .attr("x", function(d){
+      return xPositionScale (d.message)
+    })
+    .attr("dx",10)
+
+
+  // Add an x-Axis
+  var xAxis = d3.axisBottom(xPositionScale)
+    .tickFormat(d3.format("d")) // Remove the thousands marker
+    svg.append("g")
+      .attr("fill","black")
+      .attr("transform", "translate(0," + (height)+ ")")
+      .call(xAxis)
+    // There is some custom styling going on down here:
+      .select(".domain")
+        .remove() // Removes the axis vertical line
+    // And a nice little label to it!
+    svg.append("text")
+      .attr("class","x-axis-label")
+      .text("Messages sent")
+      .attr("x",width/2)
+      .attr("y",height + margin.bottom)
+      .attr("dy",-10)
+      .attr("font-weight","bold ")
+      .attr("font-size", 12)
+      .attr("text-anchor","middle")
+
+  }
+})();
---
+++ docs/whatsapp-group/chart-2.js
@@ -0,0 +1,162 @@
+(function() {
+  // Set values for the margins, height and width
+  var margin = { top: 20, left: 100, right: 20, bottom: 60},
+      height = 500 - margin.top - margin.bottom,
+      width = 500 - margin.left - margin.right;
+
+  // Create a SVG to hold the chart
+  var svg = d3.select("#chart-2-chart")
+      .append("svg")
+      .attr("height", height + margin.top + margin.bottom)
+      .attr("width", width + margin.left + margin.right)
+
+  // First, for the length of each pie chart
+  var lengthScale = d3.scaleLinear()
+    // .domain() will go from zero to the maximum value, so it will be set later
+    .range([5, height/2]) // Maximum height will occupy half of the svg. It starts at 5 to make for the innerRadius
+
+  var colorScale = d3.scaleLinear()
+    .range(["#EEBFE4","#7E0C39"])
+
+  /* I need use FOUR linear color scales, one for each part of the day
+  so I can color the chart accordingly:
+  a) midnight-dawn
+  b) dawn-noon
+  c) noon-twilight
+  d) twilight-midnight */
+
+  var dawnScale = d3.scaleOrdinal()
+    .domain(["00","01","02","03","04","05","06"])
+    .range(["#0F2F57","#210D49", "#55196D","#912989", "#B53CTF", "#DA546C"]) // Deep blue - light pink
+
+  var noonScale = d3.scaleOrdinal()
+    .domain(["07","08","09","10","11","12"])
+    .range(["#CA8686","#BF7998","#AA96BC","#9AACD7","#95B8E0", "#8BC3F2"]) // Light pink - light blue
+
+  var twilightScale = d3.scaleOrdinal()
+    .domain(["13", "14", "15", "16", "17", "18"])
+    .range(["#90AAD2","#9592B3","#9A7993", "#9F6174", "#A44854", "#8C3341"]) // Light blue - dark red
+
+  var midnightScale = d3.scaleOrdinal()
+    .domain(["19", "20", "21", "22", "23"])
+    .range(["#7D3547", "#6F374E", "#402F4E", "#273158", "#0F2F59"]) // Dark red - deep blue 
+
+  // A pie generator for drawing the slices
+  var pie = d3.pie()
+    .value(1/24)
+    .sort(null) // Don't sort by value - let the slices go in the order that the months appear in the .csv
+
+  // Crate a recipe for the arc
+  var arc = d3.arc()
+    .innerRadius(5) // The inner radius starts at 5, so we have a dot in the center
+    .outerRadius(function(d) { // But the ending point of the wedge is related to the monthly temperature
+        return lengthScale(d.data.message)
+    }) 
+
+// This seond recipe determines how the arc starts in the beginning of a transiction
+  var arcStart = d3.arc()
+    .innerRadius(5) // The inner radius starts at 5, so we have a dot in the center
+    .outerRadius(5)
+
+  // Magic invocation for the data:
+  // "OH HOLY D3.JS, LIBRARY OF LIBRARIES, THE LORD OF ALL DATAVIZ PACKAGES, I OFFER THEE A CSV FILE"
+  d3.queue()
+    .defer(d3.csv, "data/hours.csv")
+    .await(ready)
+
+  function ready(error, datapoints) {
+    // Use d3.map and d3.max to set up the missing domain
+    maxMessages = d3.max(datapoints.map(function(d){ return +d.message} )) // The + here reminds d3 that we are not dealing with strings
+    lengthScale.domain([0, maxMessages])
+    colorScale.domain([0, maxMessages])
+
+    // Create a container so I can put the 'g' inside, 
+    // in order to center the pie on it
+    var container = svg.append("g")
+      .attr("transform", "translate(360,160)")
+
+  // Take container.
+  // Add the 24 pie-slices to it. (They are the pie(datapoints))
+  // Use the arc recipe - whose height is taken from the lengthScale - to get a pie-like shape.
+  console.log(pie(datapoints))
+  container.selectAll("path")
+        .data(pie(datapoints))
+        .enter().append("path")
+        .attr("class", function(d){
+          return d.data.hour
+        })
+        .attr("d", function(d) {
+          return arcStart(d)
+        })        
+        .transition()
+        .duration(1000)
+        .attr("d", function(d) {
+          return arc(d)
+        })
+        /*.attr("fill", function(d) {
+          return colorScale(d.data.message)
+        })*/
+        .attr("stroke","white")
+        .attr("stroke-width",1)
+        .attr("fill", function(d) {
+          if (d.data.hour === "00" || d.data.hour === "01" || d.data.hour === "02" || d.data.hour === "03" || 
+            d.data.hour === "04" || d.data.hour === "05" || d.data.hour === "06" ) {
+              return dawnScale(d.data.hour)
+          }
+          else if (d.data.hour === "07" || d.data.hour === "08" || d.data.hour === "09" || d.data.hour === "10" || 
+            d.data.hour === "11" || d.data.hour === "12" ) {
+              return noonScale(d.data.hour)
+          }
+          else if (d.data.hour === "13" || d.data.hour === "14" || d.data.hour === "15" || d.data.hour === "16" || 
+            d.data.hour === "17" || d.data.hour === "18" ) {
+              return twilightScale(d.data.hour)
+          }
+          else {
+            return midnightScale(d.data.hour)
+          }
+        })
+
+  // Adding some marks to our weeeeeird clock
+  svg.append("text")
+    .attr("class", "clock-label")
+    .text("12 AM")
+    .attr("x", 350 )
+    .attr("y", height)
+    .attr("dy", -30)
+    .attr("text-anchor", "middle")
+
+  svg.append("text")
+    .attr("class", "clock-label")
+    .text("12 PM")
+    .attr("x", 350 )
+    .attr("y", 0)
+    .attr("dy", 30)
+    .attr("text-anchor", "middle")
+
+  svg.append("text")
+    .attr("class", "clock-label")
+    .text("6 PM")
+    .attr("x", 50)
+    .attr("y", 170)
+    .attr("dx", 60)
+    .attr("text-anchor", "middle")
+
+  svg.append("text")
+    .attr("class", "clock-label")
+    .text("6 PM")
+    .attr("x", 50)
+    .attr("y", 170)
+    .attr("dx", 60)
+    .attr("text-anchor", "middle")
+
+  svg.append("text")
+    .attr("class", "clock-label")
+    .text("6 AM")
+    .attr("x", width + 30)
+    .attr("y", 170)
+    .attr("text-anchor", "middle")
+
+
+  }
+
+})();
